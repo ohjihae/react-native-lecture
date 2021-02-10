7161756ad71ccfae28740e4f2fa92d6e71940922
ohjihae <ohjihae0121@naver.com>
2021-02-10 15:09:02 +0900

    react-native componet

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//import Counter from './componets/Counter';
+//import LotsOfGreetings from './componets/LotsOfGreeting';
+//import HelloWorld from './componets/HelloWorld';
+import SimpleList from './componets/SimpleList';
+// from 자바스크립트 파일
+// from 디렉토리명 -> 디렉토리/index.js
 
+
+
+/* App 컴포넌트 시작 */
 export default function App() {
+  // JSX
+  // UI 템플릿을 작성
   return (
+        // 속성={값}
+        // 속성={"문자열"}
+        // 객체 구조로 넣고싶으면 속성={{name:"sss", phone:"aaa"}}
+        // 속성={객체변수}
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <HelloWorld></HelloWorld> */}
+      {/* <Counter></Counter> */}
+      {/*<LotsOfGreetings></LotsOfGreetings>*/}
+      <SimpleList></SimpleList>
       <StatusBar style="auto" />
     </View>
+
   );
 }
+/* App 컴포넌트 끝 */
 
 const styles = StyleSheet.create({
   container: {
---
+++ componets/SelectedItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+// 선택한 아이템 한개를 속성으로 내려 받음
+const SelectedItem = ({item}) => {
+
+  return (
+    <View>
+      <Text>{item.id} {item.title}</Text>
+    </View>
+  )
+}
+
+export default SelectedItem;
